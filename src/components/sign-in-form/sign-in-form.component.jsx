5a02c876da5e66ffa67fdee5b5656435c76f7372
Victor Gavojdea <Victor.Gavojdea@gmail.com>
2023-12-02 15:55:59 -0800

    created signOut method and consolidated user calls and leveraged firebase more

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 
+import Button from '../button/button.component';
+import FormInput from '../form-input/form-input.component';
+
 import { 
     signInWithGooglePopup,
     createUserDocumentFromAuth,
     signInAuthUserWithEmailAndPassword
 } from '../../utils/firebase/firebase.utils';
-
-import FormInput from '../form-input/form-input.component';
-
-import Button from '../button/button.component';
 
 import './sign-in-form.styles.scss'
 
@@ -18,28 +17,24 @@ const defaultFormFields = {
 }
 
 const SignInForm = () => {
-    const signInWithGoogle = async () => {
-        const {user} = await signInWithGooglePopup();
-        await createUserDocumentFromAuth(user);
-    };
-
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
-
-    console.log(formFields)
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
 
+    const signInWithGoogle = async () => {
+        await signInWithGooglePopup();
+    };
+
+
     const handleSubmit = async (event) => {
         event. preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(response);
+            const {user} = await signInAuthUserWithEmailAndPassword(email, password);
             resetFormFields();
-            
         } catch(error) {
             switch(error.code) {
                 case 'auth/invalid-credential':
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,17 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet, Link } from "react-router-dom";
+
+
 import Logo from '../../assets/logo.png'; // Direct import of the PNG
+import { UserContext } from '../../contexts/user.context'
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss'
 
 const Navigation = () => {
-    return (
+  const { currentUser } = useContext(UserContext);
+
+  return (
       <Fragment>
         <div className='navigation'>
           <Link className='logo-container' to='/'>
@@ -15,9 +21,15 @@ const Navigation = () => {
             <Link className='nav-link' to='/shop'>
               SHOP
             </Link>
-            <Link className='nav-link' to='/auth'>
-              SIGN IN
-            </Link>
+            {
+              currentUser ? (
+                <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
+              ) : (
+                <Link className='nav-link' to='/auth'>
+                  SIGN IN
+                </Link>
+              )
+            }
           </div>
         </div>
         <Outlet />
