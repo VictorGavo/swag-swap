80dcdb5017bc0c13eb7977b73c2f6ee4b869a9b9
Victor Gavojdea <Victor.Gavojdea@gmail.com>
2023-12-04 15:34:36 -0800

    Added shopping cart icon and togglable in corner

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,19 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import Logo from '../../assets/logo.png'; // Direct import of the PNG
 import { UserContext } from '../../contexts/user.context'
+import { CartContext } from '../../contexts/cart.context';
+import Logo from '../../assets/logo.png'; // Direct import of the PNG
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss'
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
       <Fragment>
@@ -30,7 +34,9 @@ const Navigation = () => {
                 </Link>
               )
             }
+            <CartIcon />
           </div>
+          {isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </Fragment>
