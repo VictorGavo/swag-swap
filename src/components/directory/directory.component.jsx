cafa245b9d192a2d50d7d3c144b8cbc5fa3f16ad
Victor Gavojdea <Victor.Gavojdea@gmail.com>
2023-12-07 16:48:49 -0800

    used imagery more inline with instructors. Added bug catching for refreshed pages

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,32 +5,32 @@ const categories = [
   {
     id: 1,
     title: 'hats',
-    imageUrl: '/images/hats.webp',
-    route: 'shop/hats'
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    route: 'shop/hats',
   },
   {
     id: 2,
     title: 'jackets',
-    imageUrl: '/images/jackets.webp',
-    route: 'shop/jackets'
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    route: 'shop/jackets',
   },
   {
     id: 3,
-    title: 'footwear',
-    imageUrl: '/images/footwear.webp',
-    route: 'shop/footwear'
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    route: 'shop/sneakers',
   },
   {
     id: 4,
     title: 'womens',
-    imageUrl: '/images/womens.webp',
-    route: 'shop/womens'
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    route: 'shop/womens',
   },
   {
     id: 5,
     title: 'mens',
-    imageUrl: '/images/mens.webp',
-    route: 'shop/mens'
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    route: 'shop/mens',
   },
 ];
 
